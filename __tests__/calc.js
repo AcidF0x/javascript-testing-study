9943af20382d5047f1e9e5042fdd2dd0bbdfbd31
조두희 <ic_jdh@me.com>
2020-07-02 21:09:27 +0900

    두가지 테스트타입

--- __tests__/calc.js
+++ __tests__/calc.js
@@ -11,3 +11,44 @@ test('더하기 테스트', () => {
   // Then
   expect(result).toBe(num1 + num2);
 })
+
+describe('계산기 테스트', () => {
+  it('덧셈을 할 수 있다', () => {
+    // Given
+    const num1 = 1;
+    const num2 = 2;
+
+    // When
+    const result = calc.sum(num1, num2);
+
+    // Then
+    expect(result).toBe(num1 + num2);
+  });
+
+  describe('나눗셈 테스트', () => {
+    it('나눗셈을 할 수 있다', () => {
+      // Given
+      const num1 = 4;
+      const num2 = 2;
+
+      // When
+      const result = calc.div(num1, num2);
+
+      // Then
+      expect(result).toEqual(num1 / num2);
+    })
+    it('0으로 나눌수 없다', () => {
+      // Given
+      const number = 100;
+
+      // When && Then
+      expect(() => {calc.div(number, 0)}).toThrow('0으로 나눌수 없습니다.');
+      expect(() => {calc.div(0, number)}).toThrow('0으로 나눌수 없습니다.');
+
+      /**
+       * expect(calc.div(0, number)).toThrow('0으로 나눌수 없습니다.');
+       * 위 코드는 예외를 Jest가 잡을수 없다.
+       */
+    })
+  });
+});
