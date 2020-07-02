e69a9482941f0ce4d2f418069a2a1610a3e6a386
조두희 <ic_jdh@me.com>
2020-07-02 20:32:17 +0900

    JEST Setting

---
+++ src/calc.test.js
@@ -0,0 +1,13 @@
+const calc = require('./calc.js');
+
+test('더하기 테스트', () => {
+  // Given
+  const num1 = 1;
+  const num2 = 2;
+
+  // When
+  const result = calc.sum(num1, num2);
+
+  // Then
+  expect(result).toBe(num1 + num2);
+})
